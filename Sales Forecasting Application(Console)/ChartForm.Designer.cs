c37af2af6cf07b09c3876095ea94c3d202eed903
nitis <nitis@LAPTOP-D571AVIA>
2024-06-13 03:02:37 +0530

    Add project files.

---
+++ Sales Forecasting Application(Console)/ChartForm.Designer.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using LiveCharts;
+using LiveCharts.Wpf;
+using Sales_Forecasting_application_Console;
+
+namespace Sales_Forecasting_Application_Console_
+{
+    public partial class ChartForm : Form
+    {
+        public ChartForm(List<SalesRecord> salesRecords, decimal totalSales, decimal forecastedTotalSales)
+        {
+            InitializeComponent();
+
+            // Aggregate chart
+            cartesianChart1.Series = new SeriesCollection
+            {
+                new ColumnSeries
+                {
+                    Title = "Seeding Year Sales",
+                    Values = new ChartValues<decimal> { totalSales }
+                },
+                new ColumnSeries
+                {
+                    Title = "Forecasted Year Sales",
+                    Values = new ChartValues<decimal> { forecastedTotalSales }
+                }
+            };
+
+            cartesianChart1.AxisX.Add(new Axis
+            {
+                Labels = new[] { "Total Sales" }
+            });
+            cartesianChart1.AxisY.Add(new Axis
+            {
+                Title = "Sales",
+                LabelFormatter = value => value.ToString("C")
+            });
+
+            // Breakdown by state chart
+            var states = new List<string>();
+            var seedingSales = new ChartValues<decimal>();
+            var forecastedSales = new ChartValues<decimal>();
+
+            foreach (var record in salesRecords)
+            {
+                states.Add(record.State);
+                seedingSales.Add(record.TotalSales);
+                forecastedSales.Add(record.IncrementedSales);
+            }
+
+            cartesianChart2.Series = new SeriesCollection
+            {
+                new ColumnSeries
+                {
+                    Title = "Seeding Year Sales",
+                    Values = seedingSales
+                },
+                new ColumnSeries
+                {
+                    Title = "Forecasted Year Sales",
+                    Values = forecastedSales
+                }
+            };
+
+            cartesianChart2.AxisX.Add(new Axis
+            {
+                Labels = states.ToArray()
+            });
+            cartesianChart2.AxisY.Add(new Axis
+            {
+                Title = "Sales",
+                LabelFormatter = value => value.ToString("C")
+            });
+        }
+
+        private void InitializeComponent()
+        {
+            this.cartesianChart1 = new LiveCharts.WinForms.CartesianChart();
+            this.cartesianChart2 = new LiveCharts.WinForms.CartesianChart();
+            this.SuspendLayout();
+            // cartesianChart1
+            this.cartesianChart1.Location = new System.Drawing.Point(12, 12);
+            this.cartesianChart1.Name = "cartesianChart1";
+            this.cartesianChart1.Size = new System.Drawing.Size(776, 200);
+            this.cartesianChart1.TabIndex = 0;
+            this.cartesianChart1.Text = "cartesianChart1";
+            // cartesianChart2
+            this.cartesianChart2.Location = new System.Drawing.Point(12, 218);
+            this.cartesianChart2.Name = "cartesianChart2";
+            this.cartesianChart2.Size = new System.Drawing.Size(776, 220);
+            this.cartesianChart2.TabIndex = 1;
+            this.cartesianChart2.Text = "cartesianChart2";
+            // ChartForm
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cartesianChart2);
+            this.Controls.Add(this.cartesianChart1);
+            this.Name = "ChartForm";
+            this.Text = "Sales Forecasting Charts";
+            this.ResumeLayout(false);
+
+        }
+
+        private LiveCharts.WinForms.CartesianChart cartesianChart1;
+        private LiveCharts.WinForms.CartesianChart cartesianChart2;
+    }
+}
---
+++ Sales Forecasting Application(Console)/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using LiveCharts;
+using LiveCharts.WinForms;
+using LiveCharts.Wpf;
+using System.Data.SqlClient;
+using Sales_Forecasting_Application_Console_;
+
+namespace Sales_Forecasting_application_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Server=LAPTOP-D571AINA;Database=SuperstoreDB;User Id=username;Password=password;";
+            SalesData salesData = new SalesData(connectionString);
+
+            Console.WriteLine("Enter the year to query sales data:");
+            int year = int.Parse(Console.ReadLine());
+
+            List<SalesRecord> salesRecords = salesData.GetSalesData(year);
+
+            decimal totalSales = salesRecords.Sum(record => record.TotalSales);
+            Console.WriteLine("Total Sales for the year " + year + ": " + totalSales);
+
+            Console.WriteLine("Enter the percentage increment:");
+            decimal incrementPercentage = decimal.Parse(Console.ReadLine());
+
+            foreach (var record in salesRecords)
+            {
+                record.IncrementedSales = record.TotalSales * (1 + (incrementPercentage / 100));
+            }
+
+            decimal forecastedTotalSales = salesRecords.Sum(record => record.IncrementedSales);
+            Console.WriteLine("Total Forecasted Sales for the year " + (year + 1) + ": " + forecastedTotalSales);
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new ChartForm(salesRecords, totalSales, forecastedTotalSales));
+        }
+    }
+
+    public class SalesData
+    {
+        private string connectionString;
+
+        public SalesData(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public List<SalesRecord> GetSalesData(int year)
+        {
+            var salesData = new List<SalesRecord>();
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = @"Query 1";
+
+                using (var command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@year", year);
+
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            salesData.Add(new SalesRecord
+                            {
+                                State = reader["State"].ToString(),
+                                TotalSales = Convert.ToDecimal(reader["TotalSales"])
+                            });
+                        }
+                    }
+                }
+            }
+
+            return salesData;
+        }
+    }
+
+    public class SalesRecord
+    {
+        public string State { get; set; }
+        public decimal TotalSales { get; set; }
+        public decimal IncrementedSales { get; set; }
+    }
+}
